8c8ab46a028d40424812fd53885722cf013a61bf
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-03-11 01:04:22 +0330

    feat : add sum feature

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ function App() {
   const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
   const [thirdNumber, setThirdNumber] = useState(0);
+  const [forthNumber, setForthNumber] = useState(0);
+  const [fifthNumber, setFifthNumber] = useState(0);
 
   return (
     <>
       <h1>Salam!</h1>
+      <Typography>Please Enter Your Number:</Typography>
+      <TextField variant='outlined' value={forthNumber} onChange={(e) => setForthNumber(e.target.value)} />
+      <TextField variant='outlined' value={fifthNumber} onChange={(e) => setFifthNumber(e.target.value)} />
+      <Typography>{`Result: ${forthNumber} + ${fifthNumber} = ${forthNumber + fifthNumber}`}</Typography>
+      <br />
       <Typography>Please Enter Your Number:</Typography>
       <TextField variant='outlined' value={thirdNumber} onChange={(e) => setThirdNumber(e.target.value)} />
       <Typography>{`Result: 5 × ${thirdNumber} = ${5 * thirdNumber}`}</Typography>
