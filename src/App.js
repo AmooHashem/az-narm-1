30bba2ad53c2c79d6720fe65a273c08edac8c65c
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-03-11 01:02:00 +0330

    feat : add 5x feature

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import { useState } from 'react';
 function App() {
   const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
+  const [thirdNumber, setThirdNumber] = useState(0);
 
   return (
     <>
       <h1>Salam!</h1>
+      <Typography>Please Enter Your Number:</Typography>
+      <TextField variant='outlined' value={thirdNumber} onChange={(e) => setThirdNumber(e.target.value)} />
+      <Typography>{`Result: 5 × ${thirdNumber} = ${5 * thirdNumber}`}</Typography>
+      <br />
       <Typography>Please Enter Your Number:</Typography>
       <TextField variant='outlined' value={firstNumber} onChange={(e) => setFirstNumber(e.target.value)} />
       <Typography>{`Result: 2 × ${firstNumber} = ${2 * firstNumber}`}</Typography>
@@ -15,6 +20,7 @@ function App() {
       <Typography>Please Enter Your Number:</Typography>
       <TextField variant='outlined' value={secondNumber} onChange={(e) => setSecondNumber(e.target.value)} />
       <Typography>{`Result: 3 × ${secondNumber} = ${3 * secondNumber}`}</Typography>
+      <br />
     </>
   );
 }
